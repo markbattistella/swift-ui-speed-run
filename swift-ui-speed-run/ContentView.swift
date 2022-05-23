d83ef91a15fd6abf83b7c47bac698a22b21d5956
Mark Battistella <markbattistella@users.noreply.github.com>
2022-05-23 19:11:39 +1000

    Initial Commit

---
+++ swift-ui-speed-run/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-speed-run
+//
+//  Created by Mark Battistella on 23/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-ui-speed-run/swift_ui_speed_runApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_speed_runApp.swift
+//  swift-ui-speed-run
+//
+//  Created by Mark Battistella on 23/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_speed_runApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
